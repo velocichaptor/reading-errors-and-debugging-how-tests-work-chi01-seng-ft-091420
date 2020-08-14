ef1817a5ceb97acf929c17cc7d8a5cea65a40643
Jake Chappelle <jake.m.chappelle@gmail.com>
2020-08-14 16:14:08 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,13 @@
 # Add your variables here
+
+first_number = 2
+
+second_number = 3
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
